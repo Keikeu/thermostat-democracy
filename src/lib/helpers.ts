57342bad49070242f03f52a5d0ff5c525dcd3378
Keikeu <karolina.m.placek@gmail.com>
2026-03-03 21:04:33 +0100

    feat: add missing month name map

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -325,3 +325,18 @@ export function getThermostatRangePosition(
 
   return ((value - min) / (max - min)) * 100;
 }
+
+export const monthNameMap: Record<number, string> = {
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
+};
